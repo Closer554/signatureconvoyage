738def7f275f19414601e2e3addf80df47d1562b
Closer554 <pro1.mediago@gmail.com>
2026-07-20 00:31:53 +0200

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,8 @@
 import type { NextConfig } from "next";
+
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+
+initOpenNextCloudflareForDev();
+
 const nextConfig: NextConfig = { reactStrictMode: true };
 export default nextConfig;
